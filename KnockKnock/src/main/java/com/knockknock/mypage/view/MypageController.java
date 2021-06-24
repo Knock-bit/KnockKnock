bda01b950a0211af5b6bbfb59a408505abf69b06
hyemin <hhm930415@gmail.com>
2021-06-24 19:18:54 +0900

    #5 컨트롤러 수정 및 내정보수정페이지 생성만 해둠

--- KnockKnock/src/main/java/com/knockknock/mypage/view/MypageController.java
+++ KnockKnock/src/main/java/com/knockknock/mypage/view/MypageController.java
@@ -3,23 +3,27 @@ package com.knockknock.mypage.view;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.knockknock.mypage.service.MypageService;
 import com.knockknock.user.UserVO;
 
 @Controller
+@SessionAttributes("user")
 public class MypageController {
 	@Autowired
 	private MypageService mypageService;
 	
 	// 내 정보 수정으로 이동(임시)
+	@GetMapping("/mypage.do")
 	public String updateMypage(UserVO vo, Model model) {
 		
 		String id = "test";
 		UserVO user = mypageService.selelctOneUser(id);
 		model.addAttribute("user", user);
 		
-		return null;
+		return "updateMyInfo";
 		
 	}
 
---
+++ KnockKnock/src/main/webapp/WEB-INF/view/mypage/mypageList/updateMyInfo.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<div>
+
+	<div>
+		<ul>
+			<li>이름 : ${user.uName }</li>
+			<li>이메일 : ${user.uEmail }</li>
+		
+		
+		</ul>
+	</div>
+
+
+
+
+</div>
+
+</body>
+</html>
