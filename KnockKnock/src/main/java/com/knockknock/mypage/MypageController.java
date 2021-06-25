aec0999b8811fca4b66532beed20304533bcc473
HanJeongSeol <hanjeong94@naver.com>
2021-06-25 18:24:35 +0900

    maypageController 수정

--- KnockKnock/src/main/java/com/knockknock/mypage/MypageController.java
+++ KnockKnock/src/main/java/com/knockknock/mypage/MypageController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.knockknock.user.UserVO;
@@ -13,27 +11,24 @@ import com.knockknock.user.UserVO;
 @Controller
 @SessionAttributes("users")
 public class MypageController {
-	@Autowired
-	private MypageService mypageService;
-	
-	// 마이페이지 임시 메인으로 이동
-	@GetMapping("/myPage.do")
-	public String moveMypage() {
-		
-		return "/mypage/mypage";
-	}
-	
-	
-	// 내 정보 수정으로 이동
-	@GetMapping("/updateMyInfo.do")
-	public String updateMypage(UserVO vo, Model model) {
-		
-		String id = "test";
-		UserVO user = mypageService.selelctOneUser(id);
-		model.addAttribute("user", user);
-		
-		return "/mypage/updateMyInfo";
-		
-	}
+   @Autowired
+   private MypageService mypageService;
+   
+   // 마이페이지 임시 메인으로 이동
+   @GetMapping("/myPage.do")
+   public String moveMypage(Model model) {
+      int uIdx = 1;
+      UserVO user = mypageService.selectOneUser(uIdx);
+      model.addAttribute("users", user);
+      return "/mypage/mypage";
+   }
+   
+   // 내 정보 수정으로 이동
+   @GetMapping("/updateMyInfo.do")
+   public String updateMypage(UserVO vo, Model model) {
+      
+      model.addAttribute("users");
+      return "/mypage/mypageList/updateMyInfo";
+   }
 
 }
