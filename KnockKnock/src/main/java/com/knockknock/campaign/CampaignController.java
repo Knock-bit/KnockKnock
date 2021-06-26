165cd29f4754caa4bd950b293d0b492228aa84f3
Dayook <lidomex@gmail.com>
2021-06-27 04:13:03 +0900

    #7 campaign controller - 이동

--- KnockKnock/src/main/java/com/knockknock/campaign/CampaignController.java
+++ KnockKnock/src/main/java/com/knockknock/campaign/CampaignController.java
@@ -2,13 +2,30 @@ package com.knockknock.campaign;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CampaignController {
 	
-//	@GetMapping("campaign")
-//	public String moveCampaign() {
-//		
-//		return "cam
-//	}
+	
+	@GetMapping("/campaign/tempIndex.do")
+	public String moveTemp() {
+		System.out.println(">>> temp index로 이동");
+		return "/campaign/tempIndex";
+	}
+	
+	@GetMapping("/campaign/ing/list.do")
+	public String list() {
+		System.out.println(">>> 진행중인 캠페인으로 이동");
+		return "/campaign/ing/list";
+	}
+	
+	@GetMapping("/campaign/ing/detail.do")
+	public String detail() {
+		System.out.println(">>> 진행중 캠페인 상세페이지로 이동합니다.");
+		return "/campaign/ing/detail";
+	}
+	
+	
+	
 }
