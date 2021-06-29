1585545942843afd4f34799203a1573dbcfaad3a
HanJeongSeol <hanjeong94@naver.com>
2021-06-29 14:54:41 +0900

    #1 DB 수정으로 인한 FundingVO 변경

--- KnockKnock/src/main/java/com/knockknock/campaign/funding/FundingVO.java
+++ KnockKnock/src/main/java/com/knockknock/campaign/funding/FundingVO.java
@@ -12,20 +12,18 @@ public class FundingVO {
 	private Date cfStarttime;
 	private int cfGoalpoint;
 	private int cfCollected;
-	private char cfStatus;
+	private String cfStatus;
 	private String cfFile;
-	private int cfCategory;
-	private int hostIdx;
-	private String hostNickname;
-	
-	public FundingVO() {
-	}
+	private int ccIdx;  // 캠페인 카테고리 
+	private int uIdx;   // 유저번호	
 	
 	
-
+	
+	
+	public FundingVO() {};
+	
 	public FundingVO(int cfIdx, int cpIdx, String cfGoal, String cfTitle, String cfContent, Date cfEndtime,
-			Date cfStarttime, int cfGoalpoint, int cfCollected, char cfStatus, String cfFile, int cfCategory,
-			int hostIdx, String hostNickname) {
+			Date cfStarttime, int cfGoalpoint, int cfCollected, String cfStatus, String cfFile, int ccIdx, int uIdx) {
 		super();
 		this.cfIdx = cfIdx;
 		this.cpIdx = cpIdx;
@@ -38,131 +36,100 @@ public class FundingVO {
 		this.cfCollected = cfCollected;
 		this.cfStatus = cfStatus;
 		this.cfFile = cfFile;
-		this.cfCategory = cfCategory;
-		this.hostIdx = hostIdx;
-		this.hostNickname = hostNickname;
+		this.ccIdx = ccIdx;
+		this.uIdx = uIdx;
 	}
-
-
-
+	
+	
 	public int getCfIdx() {
 		return cfIdx;
 	}
-
 	public void setCfIdx(int cfIdx) {
 		this.cfIdx = cfIdx;
 	}
-
 	public int getCpIdx() {
 		return cpIdx;
 	}
-
 	public void setCpIdx(int cpIdx) {
 		this.cpIdx = cpIdx;
 	}
-
 	public String getCfGoal() {
 		return cfGoal;
 	}
-
 	public void setCfGoal(String cfGoal) {
 		this.cfGoal = cfGoal;
 	}
-
 	public String getCfTitle() {
 		return cfTitle;
 	}
-
 	public void setCfTitle(String cfTitle) {
 		this.cfTitle = cfTitle;
 	}
-
 	public String getCfContent() {
 		return cfContent;
 	}
-
 	public void setCfContent(String cfContent) {
 		this.cfContent = cfContent;
 	}
-
 	public Date getCfEndtime() {
 		return cfEndtime;
 	}
-
 	public void setCfEndtime(Date cfEndtime) {
 		this.cfEndtime = cfEndtime;
 	}
-
 	public Date getCfStarttime() {
 		return cfStarttime;
 	}
-
 	public void setCfStarttime(Date cfStarttime) {
 		this.cfStarttime = cfStarttime;
 	}
-
 	public int getCfGoalpoint() {
 		return cfGoalpoint;
 	}
-
 	public void setCfGoalpoint(int cfGoalpoint) {
 		this.cfGoalpoint = cfGoalpoint;
 	}
-
 	public int getCfCollected() {
 		return cfCollected;
 	}
-
 	public void setCfCollected(int cfCollected) {
 		this.cfCollected = cfCollected;
 	}
-
-	public char getCfStatus() {
+	public String getCfStatus() {
 		return cfStatus;
 	}
-
-	public void setCfStatus(char cfStatus) {
+	public void setCfStatus(String cfStatus) {
 		this.cfStatus = cfStatus;
 	}
-
 	public String getCfFile() {
 		return cfFile;
 	}
-
 	public void setCfFile(String cfFile) {
 		this.cfFile = cfFile;
 	}
-
-	public int getCfCategory() {
-		return cfCategory;
+	public int getCcIdx() {
+		return ccIdx;
 	}
-
-	public void setCfCategory(int cfCategory) {
-		this.cfCategory = cfCategory;
+	public void setCcIdx(int ccIdx) {
+		this.ccIdx = ccIdx;
 	}
-
-	public int getHostIdx() {
-		return hostIdx;
+	public int getuIdx() {
+		return uIdx;
 	}
-
-	public void setHostIdx(int hostIdx) {
-		this.hostIdx = hostIdx;
+	public void setuIdx(int uIdx) {
+		this.uIdx = uIdx;
 	}
-
-
-
-	public String getHostNickname() {
-		return hostNickname;
-	}
-
-
-
-	public void setHostNickname(String hostNickname) {
-		this.hostNickname = hostNickname;
+	@Override
+	public String toString() {
+		return "FundingVO [cfIdx=" + cfIdx + ", cpIdx=" + cpIdx + ", cfGoal=" + cfGoal + ", cfTitle=" + cfTitle
+				+ ", cfContent=" + cfContent + ", cfEndtime=" + cfEndtime + ", cfStarttime=" + cfStarttime
+				+ ", cfGoalpoint=" + cfGoalpoint + ", cfCollected=" + cfCollected + ", cfStatus=" + cfStatus
+				+ ", cfFile=" + cfFile + ", ccIdx=" + ccIdx + ", uIdx=" + uIdx + "]";
 	}
 	
 	
+	
 
-	
-	
 }
+
+
