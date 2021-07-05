8efaeaa21dd670cd0a903e9a7626889bfbe3acd9
dkim0808 <dakim0808@gmail.com>
2021-07-05 13:08:03 +0900

    [16] product파일 오류 수정

--- KnockKnock/src/main/java/com/knockknock/product/ProductService.java
+++ KnockKnock/src/main/java/com/knockknock/product/ProductService.java
@@ -2,8 +2,6 @@ package com.knockknock.product;
 
 import java.util.List;
 
-public interface ProductService {
-	public List<ProductVO> selectProductList();
 import com.knockknock.util.ProductVO;
 
 public interface ProductService {
--- KnockKnock/src/main/java/com/knockknock/product/impl/ProductDAO.java
+++ KnockKnock/src/main/java/com/knockknock/product/impl/ProductDAO.java
@@ -1,6 +1,5 @@
 package com.knockknock.product.impl;
-
-public class ProductDAO {
+ 
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -8,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.knockknock.util.ProductVO;
+
 @Repository
 public class ProductDAO {
-	
+
 	@Autowired
 	private SqlSessionTemplate mybatis;
-	
+
 	public List<ProductVO> ProductList() {
 
 		return mybatis.selectList("UserVO.productList");
