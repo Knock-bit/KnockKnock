6d731d4b39e4755d781863aaee6adef423dece1a
hyemin <hhm930415@gmail.com>
2021-07-05 13:04:42 +0900

    #17 productList 위치 수정

--- KnockKnock/src/main/java/com/knockknock/product/impl/ProductDAO.java
+++ KnockKnock/src/main/java/com/knockknock/product/impl/ProductDAO.java
@@ -1,12 +1,12 @@
 package com.knockknock.product.impl;
 
-import java.util.List;
+import java.util.List; 
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.knockknock.util.ProductVO;
+import com.knockknock.product.ProductVO;
 @Repository
 public class ProductDAO {
 	
@@ -16,6 +16,12 @@ public class ProductDAO {
 	public List<ProductVO> ProductList() {
 
 		return mybatis.selectList("UserVO.productList");
+	}
+	
+	// 상품 정보 가져오기(임시)
+	public ProductVO productDetail(int pIdx) {
+
+		return mybatis.selectOne("UserVO.productDetail", pIdx);
 	}
 
 }
--- KnockKnock/src/main/java/com/knockknock/product/impl/ProductServiceImpl.java
+++ KnockKnock/src/main/java/com/knockknock/product/impl/ProductServiceImpl.java
@@ -1,12 +1,12 @@
 package com.knockknock.product.impl;
 
-import java.util.List;
+import java.util.List; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.knockknock.product.ProductService;
-import com.knockknock.util.ProductVO;
+import com.knockknock.product.ProductVO;
 
 @Service("productService")
 public class ProductServiceImpl implements ProductService{
@@ -16,5 +16,12 @@ public class ProductServiceImpl implements ProductService{
 	@Override
 	public List<ProductVO> ProductList() {
 		return productDAO.ProductList();
+	}
+	
+	// 상품정보 가져오기 (임시)
+	@Override
+	public ProductVO productDetail(int pIdx) {
+		
+		return productDAO.productDetail(pIdx);
 	}
 }
