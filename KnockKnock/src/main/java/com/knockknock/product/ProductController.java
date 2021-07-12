a57398bdb19047d52dce872ef837204df3a1dceb
HanJeongSeol <hanjeong94@naver.com>
2021-07-12 16:30:29 +0900

    ProductController 오류 수정

--- KnockKnock/src/main/java/com/knockknock/product/ProductController.java
+++ KnockKnock/src/main/java/com/knockknock/product/ProductController.java
@@ -27,181 +27,179 @@ import com.google.gson.JsonObject;
 @Controller
 @SessionAttributes("users")
 public class ProductController {
-	@Autowired
-	private ProductService productService;
-	private String uploadPath = "C:/knock2/product/";
-	ProductVO product = new ProductVO();
+   @Autowired
+   private ProductService productService;
+   private String uploadPath = "C:/knock2/product/";
+   ProductVO product = new ProductVO();
 
-	// SHOP메인페이지(전체품목 리스트로 가져오기)
-//	@GetMapping("/productlist.do")
-//	public String ProductList(Model model) {
-//		List<ProductVO> plist = productService.ProductList();
-//		model.addAttribute("plist", plist);
-//		return "/product/productlist";
-//	}
-//	
-	//카테고리별 리스트보기
-	@RequestMapping(value ="/productlist.do", method=RequestMethod.GET)
- 	public String productCategory(Model model, ProductVO vo) {
-		System.out.println("컨트롤러");
-		List<ProductVO> plist = productService.categoryProduct(vo);
-		
-		model.addAttribute("plist", plist);
-		 
-		return "product/productlist";
-	}
+   // SHOP메인페이지(전체품목 리스트로 가져오기)
+//   @GetMapping("/productlist.do")
+//   public String ProductList(Model model) {
+//      List<ProductVO> plist = productService.ProductList();
+//      model.addAttribute("plist", plist);
+//      return "/product/productlist";
+//   }
+//   
+   //카테고리별 리스트보기
+   @RequestMapping(value ="/productlist.do", method=RequestMethod.GET)
+    public String productCategory(Model model, ProductVO vo) {
+      System.out.println("컨트롤러");
+      List<ProductVO> plist = productService.categoryProduct(vo);
+      
+      model.addAttribute("plist", plist);
+       
+      return "product/productlist";
+   }
 
-	// 상품 목록(정보) 가져오기 - 임시
-	@GetMapping("/productDetail.do")
-	public String ProductDetail(Model model, int pIdx) {
+   // 상품 목록(정보) 가져오기 - 임시
+   @GetMapping("/productDetail.do")
+   public String ProductDetail(Model model, int pIdx) {
 
-		ProductVO pvo = productService.productDetail(pIdx);
-		System.out.println("pvo : " + pvo);
+      ProductVO pvo = productService.productDetail(pIdx);
+      System.out.println("pvo : " + pvo);
 
-		model.addAttribute("pvo", pvo);
-		return "/product/productDetail";
-	}
+      model.addAttribute("pvo", pvo);
+      return "/product/productDetail";
+   }
 
-	// 상품 등록(일반)
-	@PostMapping("/productRegister.do")
-	public String productRegister(ProductVO product, MultipartFile file) throws IllegalStateException, IOException {
-		System.out.println(">>>Controller");
+   // 상품 등록(일반)
+   @PostMapping("/productRegister.do")
+   public String productRegister(ProductVO product, MultipartFile file) throws IllegalStateException, IOException {
+      System.out.println(">>>Controller");
 
-		System.out.println(product);
-		if (file.isEmpty()) {
+      System.out.println(product);
+      if (file.isEmpty()) {
 
-		} else {
-			// 저장경로 지정
-			String fileName = file.getOriginalFilename();
-			String onlyFileName = fileName.substring(0, fileName.indexOf("."));
-			String extension = fileName.substring(fileName.indexOf("."));
-			String filePath = null;
-			int count = 0;
+      } else {
+         // 저장경로 지정
+         String fileName = file.getOriginalFilename();
+         String onlyFileName = fileName.substring(0, fileName.indexOf("."));
+         String extension = fileName.substring(fileName.indexOf("."));
+         String filePath = null;
+         int count = 0;
 
-			// 중복 파일명 확인
-			while (true) {
-				if (count == 0) {
-					filePath = onlyFileName + extension;
-				} else {
-					filePath = onlyFileName + "(" + count + ")" + extension;
-				}
-				System.out.println("파일 생성 ! ");
-				File checkFile = new File(uploadPath + filePath);
-				System.out.println("파일명 :" + filePath);
-				if (!checkFile.exists()) {
-					System.out.println("if문 안");
-					break;
-				}
-				System.out.println("if문 지나오나?");
-				count++;
-			}
-			file.transferTo(new File(uploadPath + filePath));
-			product.setpImg(filePath + fileName);
-		}
-		System.out.println(product.getpImg());
-		int result = productService.insertProduct(product);
-		System.out.println(result + "건 제안서 제출. ");
+         // 중복 파일명 확인
+         while (true) {
+            if (count == 0) {
+               filePath = onlyFileName + extension;
+            } else {
+               filePath = onlyFileName + "(" + count + ")" + extension;
+            }
+            System.out.println("파일 생성 ! ");
+            File checkFile = new File(uploadPath + filePath);
+            System.out.println("파일명 :" + filePath);
+            if (!checkFile.exists()) {
+               System.out.println("if문 안");
+               break;
+            }
+            System.out.println("if문 지나오나?");
+            count++;
+         }
+         file.transferTo(new File(uploadPath + filePath));
+         product.setpImg(filePath + fileName);
+      }
+      System.out.println(product.getpImg());
+      int result = productService.insertProduct(product);
+      System.out.println(result + "건 제안서 제출. ");
 
-		return "/seller/product/productDetail";
-	}
+      return "/seller/product/productDetail";
+   }
 
-	@GetMapping("/productForm.do")
-	public String productForm() {
-		return "seller/product/productForm";
-	}
+   @GetMapping("/productForm.do")
+   public String productForm() {
+      return "seller/product/productForm";
+   }
 
-	@PostMapping("/productSummer.do")
-	public String insertProposal(ProductVO product) throws IllegalStateException, IOException {
-		System.out.println(product);
+   @PostMapping("/productSummer.do")
+   public String insertProposal(ProductVO product, MultipartFile file) throws IllegalStateException, IOException {
+      System.out.println(product);
+      if (file.isEmpty()) {
 
-		if (file.isEmpty()) {
+      } else {
+         // 저장경로 지정
+         String fileName = file.getOriginalFilename();
+         String onlyFileName = fileName.substring(0, fileName.indexOf("."));
+         String extension = fileName.substring(fileName.indexOf("."));
+         String filePath = null;
+         int count = 0;
 
-		} else {
-			// 저장경로 지정
-			String fileName = file.getOriginalFilename();
-			String onlyFileName = fileName.substring(0, fileName.indexOf("."));
-			String extension = fileName.substring(fileName.indexOf("."));
-			String filePath = null;
-			int count = 0;
+         // 중복 파일명 확인
+         while (true) {
+            if (count == 0) {
+               filePath = onlyFileName + extension;
+            } else {
+               filePath = onlyFileName + "(" + count + ")" + extension;
+            }
+            System.out.println("파일 생성 ! ");
+            File checkFile = new File(uploadPath + filePath);
+            System.out.println("파일명 :" + filePath);
+            if (!checkFile.exists()) {
+               System.out.println("if문 안");
+               break;
+            }
+            System.out.println("if문 지나오나?");
+            count++;
+         }
+         file.transferTo(new File(uploadPath + filePath));
+         product.setpImg(filePath + fileName);
+      }
+      System.out.println(product.getpImg());
+      int result = productService.insertProduct(product);
+      System.out.println(result + "건 등록. ");
 
-			// 중복 파일명 확인
-			while (true) {
-				if (count == 0) {
-					filePath = onlyFileName + extension;
-				} else {
-					filePath = onlyFileName + "(" + count + ")" + extension;
-				}
-				System.out.println("파일 생성 ! ");
-				File checkFile = new File(uploadPath + filePath);
-				System.out.println("파일명 :" + filePath);
-				if (!checkFile.exists()) {
-					System.out.println("if문 안");
-					break;
-				}
-				System.out.println("if문 지나오나?");
-				count++;
-			}
-			file.transferTo(new File(uploadPath + filePath));
-			product.setpImg(filePath + fileName);
-		}
+      return "/seller/product/productResult";
+   }
 
-		System.out.println(product.getpImg());
-		int result = productService.insertProduct(product);
-		System.out.println(result + "건 등록. ");
+   // 상품등록 - 써머노트
+   /*
+    * @PostMapping("/productSummer.do") public String insertProposal(ProductVO
+    * product) { System.out.println(product); int result =
+    * productService.insertProduct(product);
+    * 
+    * return "campaign/proposal/pResult"; }
+    */
 
-		return "/seller/product/productResult";
-	}
+   @RequestMapping(value = "/uploadProductSummernoteImageFile.do", produces = "application/json; charset=utf8")
+   @ResponseBody
+   public String uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile,
+         HttpServletRequest request) {
+      JsonObject jsonObject = new JsonObject();
 
-	// 상품등록 - 써머노트
-	/*
-	 * @PostMapping("/productSummer.do") public String insertProposal(ProductVO
-	 * product) { System.out.println(product); int result =
-	 * productService.insertProduct(product);
-	 * 
-	 * return "campaign/proposal/pResult"; }
-	 */
+      String fileRoot = "C:/knock2/summer/"; // 외부경로로 저장을 희망할때.
 
-	@RequestMapping(value = "/uploadProductSummernoteImageFile.do", produces = "application/json; charset=utf8")
-	@ResponseBody
-	public String uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile,
-			HttpServletRequest request) {
-		JsonObject jsonObject = new JsonObject();
+      // 내부경로로 저장
 
-		String fileRoot = "C:/knock2/summer/"; // 외부경로로 저장을 희망할때.
+      String originalFileName = multipartFile.getOriginalFilename(); // 오리지날 파일명
+      String extension = originalFileName.substring(originalFileName.lastIndexOf(".")); // 파일 확장자
+      String savedFileName = UUID.randomUUID() + extension; // 저장될 파일 명
 
-		// 내부경로로 저장
+      File targetFile = new File(fileRoot + savedFileName);
+      try {
+         InputStream fileStream = multipartFile.getInputStream();
+         FileUtils.copyInputStreamToFile(fileStream, targetFile); // 파일 저장
+         jsonObject.addProperty("url", "/img/" + savedFileName); // contextroot + resources + 저장할 내부 폴더명
+         jsonObject.addProperty("responseCode", "success");
+         jsonObject.addProperty("fileName", savedFileName);
 
-		String originalFileName = multipartFile.getOriginalFilename(); // 오리지날 파일명
-		String extension = originalFileName.substring(originalFileName.lastIndexOf(".")); // 파일 확장자
-		String savedFileName = UUID.randomUUID() + extension; // 저장될 파일 명
-
-		File targetFile = new File(fileRoot + savedFileName);
-		try {
-			InputStream fileStream = multipartFile.getInputStream();
-			FileUtils.copyInputStreamToFile(fileStream, targetFile); // 파일 저장
-			jsonObject.addProperty("url", "/img/" + savedFileName); // contextroot + resources + 저장할 내부 폴더명
-			jsonObject.addProperty("responseCode", "success");
-			jsonObject.addProperty("fileName", savedFileName);
-
-		} catch (IOException e) {
-			FileUtils.deleteQuietly(targetFile); // 저장된 파일 삭제
-			jsonObject.addProperty("responseCode", "error");
-			e.printStackTrace();
-		}
-		String a = jsonObject.toString();
-		System.out.println(a);
-		return a;
-	}
-	
-	@RequestMapping(value = "/seller/manageMyProducts.do", method = RequestMethod.GET)
-	public String myProducts(HttpSession session, ProductVO vo, Model model, int sIdx){
-		System.out.println("컨트롤러요");
-		System.out.println("상품조회시 사용 sIdx="+sIdx);
-		session.getAttribute("seller");
-		List<ProductVO> myproducts = productService.manageMyproducts(sIdx);
-		System.out.println("Myproduct = "+myproducts);
-		model.addAttribute("myproducts", myproducts);
-		return "/seller/product/productResult";
-	}
+      } catch (IOException e) {
+         FileUtils.deleteQuietly(targetFile); // 저장된 파일 삭제
+         jsonObject.addProperty("responseCode", "error");
+         e.printStackTrace();
+      }
+      String a = jsonObject.toString();
+      System.out.println(a);
+      return a;
+   }
+   
+   @RequestMapping(value = "/seller/manageMyProducts.do", method = RequestMethod.GET)
+   public String myProducts(HttpSession session, ProductVO vo, Model model, int sIdx){
+      System.out.println("컨트롤러요");
+      System.out.println("상품조회시 사용 sIdx="+sIdx);
+      session.getAttribute("seller");
+      List<ProductVO> myproducts = productService.manageMyproducts(sIdx);
+      System.out.println("Myproduct = "+myproducts);
+      model.addAttribute("myproducts", myproducts);
+      return "/seller/product/productResult";
+   }
 
 }
--- KnockKnock/src/main/webapp/layout/navbar/navAdmin.jsp
+++ KnockKnock/src/main/webapp/layout/navbar/navAdmin.jsp
@@ -1,58 +1,62 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<c:set var="cp" value ="${pageContext.request.contextPath }"/>
-  <!-- ======= Header ======= -->
-   <meta charset=utf-8>
-  <header id="header" class="fixed-top">
-    <div class="container d-flex align-items-center">
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<c:set var="cp" value="${pageContext.request.contextPath }" />
+<!-- ======= Header ======= -->
+<meta charset=utf-8>
+<header id="header" class="fixed-top">
+	<div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="${cp }/layout/main.jsp"><img src="${cp }/resource/img/logo.png" id=:logo></a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+		<h1 class="logo me-auto">
+			<a href="${cp }/layout/main.jsp"><img
+				src="${cp }/resource/img/logo.png" id=:logo></a>
+		</h1>
+		<!-- Uncomment below if you prefer to use an image logo -->
+		<!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a href="${cp }/run.jsp">Home</a></li>
-          <li class="dropdown"><a href="#"><span>About</span> <i class="bi bi-chevron-down"></i></a>
-              <ul>
-				<li><a href="${cp }/layout/about/whatisknock.jsp">綠!Knock은?</a></li>
-				<li><a href="${cp }/layout/about/manual.jsp">綠!Knock!이용법</a></li>
-            </ul>
-          </li>
-          <li class="dropdown"><a href="#"><span>Campaign</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="${cp }/campaign/ing/list.do">진행중인 캠페인</a></li>
-              <li><a href="${cp }/campaign/funding/list.do">캠페인 펀딩하기</a></li>
-              <li><a href="${cp }/campaign/proposal.do">캠페인 제안하기</a></li>
-            </ul>
-          </li>
-
-
-          <li><a href="${cp }/productlist.do?pcIdx=0">Shop</a></li>
-          <li><a href="${cp }/contact/contact.do">Contact</a></li>
-
-          <li class="dropdown"><a href="#"><button type="button" class="btn btn-dark">관리자모드</button> <i
-                class="bi bi-chevron-down"></i></a>
-
-            <ul>
-              <li><a href="${cp }/adminKeywordList.do">키워드 관리</a></li>
-              <li><a href="${cp }/adminProposalList.do">제안 관리</a></li>
-              <li><a href="${cp }/adminUserList.do">회원 관리</a></li>
-              <li><a href="${cp }/adminCampaignCategory.do">캠페인 카테고리 관리</a></li>
-              <li><a href="${cp }/getFundingList.do">펀딩 목록 관리</a></li>
-              <li><a href="${cp }/adminCampaignList.do">캠페인 목록 관리</a></li>
-              <li><a href="${cp }/getContactList.do">문의내역 확인</a></li>
-              <li><a href="${cp }/user/logout.do">로그아웃</a></li>
-            </ul>
-          </li>
+		<nav id="navbar" class="navbar order-last order-lg-0">
+			<ul>
+				<li><a href="${cp }/run.jsp">Home</a></li>
+				<li class="dropdown"><a href="#"><span>About</span> <i
+						class="bi bi-chevron-down"></i></a>
+					<ul>
+						<li><a href="${cp }/layout/about/whatisknock.jsp">綠!Knock은?</a></li>
+						<li><a href="${cp }/layout/about/manual.jsp">綠!Knock!이용법</a></li>
+					</ul></li>
+				<li class="dropdown"><a href="#"><span>Campaign</span> <i
+						class="bi bi-chevron-down"></i></a>
+					<ul>
+						<li><a href="${cp }/campaign/ing/list.do">진행중인 캠페인</a></li>
+						<li><a href="${cp }/campaign/funding/list.do">캠페인 펀딩하기</a></li>
+						<li><a href="${cp }/campaign/proposal.do">캠페인 제안하기</a></li>
+					</ul></li>
 
 
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
+				<li><a href="${cp }/productlist.do?pcIdx=0">Shop</a></li>
+				<li><a href="${cp }/contact/contact.do">Contact</a></li>
+
+				<li class="dropdown"><a href="#"><button type="button"
+							class="btn btn-dark">관리자모드</button> <i class="bi bi-chevron-down"></i></a>
+
+					<ul>
+						<li><a href="${cp }/adminUserList.do">회원 관리</a></li>
+						<li><a href="${cp }/adminKeywordList.do">키워드 관리</a></li>
+						<li><a href="${cp }/adminCampaignCategory.do">캠페인 카테고리 관리</a></li>
+						<li><a href="${cp }/adminProposalList.do">제안서 관리</a></li>
+						<li><a href="${cp }/getFundingList.do">펀딩 목록 관리</a></li>
+						<li><a href="${cp }/adminCampaignList.do">캠페인 목록 관리</a></li>
+						<li><a href="${cp }/getContactList.do">문의내역 확인</a></li>
+						<li><a href="${cp }/user/logout.do">로그아웃</a></li>
+					</ul></li>
+
+
+			</ul>
+			<i class="bi bi-list mobile-nav-toggle"></i>
+		</nav>
+		<!-- .navbar -->
 
 
 
-    </div>
-  </header><!-- End Header -->
+	</div>
+</header>
+<!-- End Header -->
